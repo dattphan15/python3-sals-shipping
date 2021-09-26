58078edc0b64fdac5f86b98cdfa19eb5a014df37
Kevin Phan <ken.phan@live.ca>
2021-09-26 16:31:09 -0700

    add control flow for shipping rates, compare best prices, rounded to 2 decimals

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sal's Shipping
+
+A shipping.py Python program that asks the user for the weight of their package and then tells them which method of shipping is cheapest and how much it will cost to ship their package using Sal’s Shippers.
+
+Sal’s Shippers has several different options for a customer to ship their package:
+
+- Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
+
+- Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
+
+- Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
+
+
+<br>  
+
+### Shipping Rates
+
+![rates](images/sals-shipping-rates.png)
---
+++ shipping.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+weight = 41.5
+
+
+# Ground Shipping
+cost_ground = 0
+
+if weight <= 2:
+  cost_ground = (weight * 1.50) + 20
+elif (weight > 2) and (weight <= 6):
+  cost_ground = (weight * 3.00) + 20
+elif (weight > 6) and (weight <= 10):
+  cost_ground = (weight * 4.00) + 20
+else:
+  cost_ground = (weight * 4.75) + 20
+# Convert to decimal
+cost_ground = Decimal(cost_ground)
+
+
+# Ground Shipping Premium
+cost_ground_premium = 125.00
+# convert to decimal
+cost_ground_premium = Decimal(cost_ground_premium)
+
+
+# Drone Shipping
+cost_drone_shipping = 0
+
+if weight <= 2:
+  cost_drone_shipping = (weight * 4.50)
+elif (weight > 2) and (weight <= 6):
+  cost_drone_shipping = (weight * 9.00)
+elif (weight > 6) and (weight <= 10):
+  cost_drone_shipping = (weight * 12.00)
+else:
+  cost_drone_shipping = (weight * 14.25)
+# convert to decimal
+cost_drone_shipping = Decimal(cost_drone_shipping)
+
+
+print("Ground Shipping: $" + str(round(cost_ground, 2)))
+print("Ground Shipping Premium: $" + str(round(cost_ground_premium, 2)))
+print("Ground Drone Shipping: $" + str(round(cost_drone_shipping, 2)))
